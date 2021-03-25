85ce98913db21ac8b4d2d1c300f7e6a7c7af4629
T3sultan <incognito.soul011@gmail.com>
2021-03-26 03:28:47 +0600

    showing&HadingContainer

--- index.js
+++ index.js
@@ -12,20 +12,7 @@ const StrorageCtrl = (function () {
 //taskctrl function to control data related part
 const TaskCtrl = (function () {
     let data = {
-        tasks: [
-            { id: 0, title: 'task 1', comleted: false },
-            { id: 1, title: 'task 2', comleted: false },
-            { id: 3, title: 'task 3', comleted: false },
-            { id: 4, title: 'task 4', comleted: false },
-            { id: 5, title: 'task 5', comleted: false },
-            { id: 6, title: 'task 6', comleted: false },
-            { id: 7, title: 'task 7', comleted: false },
-            { id: 8, title: 'task 8', comleted: false },
-            { id: 9, title: 'task 9', comleted: false },
-            { id: 10, title: 'task 10', comleted: false },
-            { id: 11, title: 'task 11', comleted: false },
-            { id: 12, title: 'task 12', comleted: false },
-        ]
+        tasks: []
 
     };
     return {
@@ -66,6 +53,13 @@ const UICtrl = (function () {
         taskTitle: '.task-title'
     };
 
+    const hideTaskContainer = function(){
+        document.querySelector(selectors.taskContainer).style.display='none';
+    }
+    const showTaskContainer = function(){
+        document.querySelector(selectors.taskContainer).style.display='block';
+    }
+
     return {
         getSelector() {
             return selectors;
@@ -99,6 +93,7 @@ const UICtrl = (function () {
 
         },
         populateTask(task) {
+            showTaskContainer();
             let output = '';
             output += `
             <div class="task-item" id="task-${task.id}">
@@ -122,36 +117,46 @@ const UICtrl = (function () {
 
             
             `;
-            document.querySelector(selectors.taskContainer).insertAdjacentHTML('beforeend',output);
+            document.querySelector(selectors.taskContainer).insertAdjacentHTML('beforeend', output);
 
         },
 
         populateTasks(tasks) {
-            let output = '';
-            tasks.forEach(task => {
-                output += `
-                <div class="task-item" id="task-${task.id}">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <h5>${task.title}</h5>
-                    </div>
-                    <div class="col-sm-6">
-                        <a href="#" class="completed-task float-right">
-                            <i class="fas fa-check"></i>
-                        </a>
-                        <a href="#" class="edit-task float-right mr-2">
-                            <i class="fas fa-pencil-alt"></i>
-                        </a>
+            if (tasks.length === 0) {
+                //hiding task container there is no tasks
+                hideTaskContainer();
 
+            } else {
+                //showing task container there is tasks
+                showTaskContainer();
+                let output = '';
+                tasks.forEach(task => {
+                    output += `
+                    <div class="task-item" id="task-${task.id}">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h5>${task.title}</h5>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="#" class="completed-task float-right">
+                                <i class="fas fa-check"></i>
+                            </a>
+                            <a href="#" class="edit-task float-right mr-2">
+                                <i class="fas fa-pencil-alt"></i>
+                            </a>
+    
+                        </div>
+    
                     </div>
-
+    
                 </div>
+    
+                    `;
+                });
+                document.querySelector(selectors.taskContainer).innerHTML = output;
 
-            </div>
+            }
 
-                `;
-            });
-            document.querySelector(selectors.taskContainer).innerHTML = output;
 
         }
     }
